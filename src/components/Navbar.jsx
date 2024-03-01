8f5087ee107a57d28da8e6a8993ae747dd76e3cc
yeysonpupa <yeysonpupa@unisabana.edu.co>
2024-03-01 12:38:19 -0500

    Responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [hovered, setHovered] = useState(null);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   const handleMouseEnter = (id) => {
     setHovered(id);
@@ -26,49 +37,80 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-whiteBackground p-4 md:p-8 flex justify-between items-center">
-      <div className="text-white text-xl font-bold">
-        <Link to="/" className="text-TextoGrande font-museomoderno">
+    <nav className="bg-whiteBackground p-4 md:p-8 flex justify-between items-center relative">
+      <div className="text-white md:text-xl text-TextoGrande font-bold">
+        <Link to="/" className="text-2xl md:text-TextoGrande font-museomoderno" onClick={closeMenu}>
           <span className="text-blackText font-light">{`{Esteban`}</span>
           <span className="text-primary font-light">.</span>
           <span className="text-blackText font-light">{`}`}</span>
         </Link>
       </div>
-      <div className="flex space-x-12">
-        <div className="relative inline-flex items-center">
+      <div className="md:hidden">
+        <button className="text-blackText" onClick={handleMenuToggle}>
+          <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
+        </button>
+      </div>
+      {isMenuOpen ? (
+        <div className="md:hidden fixed top-16 left-0 right-0 bottom-0 bg-whiteBackground flex flex-col items-center justify-center overflow-hidden" style={{ paddingTop: '64px' }}>
           <Link
             to="/projects"
-            className="text-TextoNormal font-manrope text-blackText"
-            onMouseEnter={() => handleMouseEnter('projects')}
-            onMouseLeave={handleMouseLeave}
+            className="text-TextoNormal font-manrope text-blackText mb-4"
+            onClick={closeMenu}
           >
-            {renderCircle('projects')}
             Projects
           </Link>
-        </div>
-        <div className="relative inline-flex items-center">
           <Link
             to="/about"
-            className="text-TextoNormal font-manrope text-blackText"
-            onMouseEnter={() => handleMouseEnter('about')}
-            onMouseLeave={handleMouseLeave}
+            className="text-TextoNormal font-manrope text-blackText mb-4"
+            onClick={closeMenu}
           >
-            {renderCircle('about')}
             About
           </Link>
-        </div>
-        <div className="relative inline-flex items-center">
           <Link
             to="/snap"
-            className="text-TextoNormal font-manrope text-blackText"
-            onMouseEnter={() => handleMouseEnter('snap')}
-            onMouseLeave={handleMouseLeave}
+            className="text-TextoNormal font-manrope text-blackText mb-4"
+            onClick={closeMenu}
           >
-            {renderCircle('snap')}
             Snap
           </Link>
         </div>
-      </div>
+      ) : (
+        <div className="hidden md:flex space-x-12">
+          <div className="relative inline-flex items-center">
+            <Link
+              to="/projects"
+              className="text-TextoNormal font-manrope text-blackText"
+              onMouseEnter={() => handleMouseEnter('projects')}
+              onMouseLeave={handleMouseLeave}
+            >
+              {renderCircle('projects')}
+              Projects
+            </Link>
+          </div>
+          <div className="relative inline-flex items-center">
+            <Link
+              to="/about"
+              className="text-TextoNormal font-manrope text-blackText"
+              onMouseEnter={() => handleMouseEnter('about')}
+              onMouseLeave={handleMouseLeave}
+            >
+              {renderCircle('about')}
+              About
+            </Link>
+          </div>
+          <div className="relative inline-flex items-center">
+            <Link
+              to="/snap"
+              className="text-TextoNormal font-manrope text-blackText"
+              onMouseEnter={() => handleMouseEnter('snap')}
+              onMouseLeave={handleMouseLeave}
+            >
+              {renderCircle('snap')}
+              Snap
+            </Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
