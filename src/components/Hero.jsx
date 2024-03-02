f714fda10c00f3c52257afc1084ee011a7620d3d
yeysonpupa <yeysonpupa@unisabana.edu.co>
2024-03-01 22:36:40 -0500

    Type animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,39 @@
+import { useState, useEffect } from 'react';
 import ArrowIcon from '../assets/images/arrow.svg';
 
 const Hero = () => {
+  const [dynamicText, setDynamicText] = useState('');
+  const words = ['Creating', 'Dreaming', 'Coding'];
+  const [wordIndex, setWordIndex] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const typingSpeed = 150; 
+  const deletingSpeed = 75; 
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const currentWord = words[wordIndex];
+      const currentText = dynamicText;
+
+      if (isDeleting) {
+        setDynamicText(currentText.slice(0, -1));
+      } else {
+        setDynamicText(currentWord.slice(0, currentText.length + 1));
+      }
+
+      if (
+        (isDeleting && currentText.length === 0) ||
+        (!isDeleting && currentText === currentWord)
+      ) {
+        setIsDeleting(!isDeleting);
+        if (!isDeleting) {
+          setWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+        }
+      }
+    }, isDeleting ? deletingSpeed : typingSpeed);
+
+    return () => clearInterval(intervalId);
+  }, [dynamicText, wordIndex, isDeleting, words]);
+
   return (
     <div className="pr-8 pl-8 pt-8 md:pt-16 bg-whiteBackground">
       <p className="font-manrope text-TextoPequeñoPhone md:text-TextoNormal text-greyText ml-1">
@@ -12,7 +45,11 @@ const Hero = () => {
       </p>
 
       <div className="flex items-center justify-center">
-        <img src={ArrowIcon} alt="Arrow Icon" className="w-15 h-15 pt-2 pl-2 pb-0 md:pt-10 md:pl-40 md:pb-5 transform scale-x-50 scale-y-50 md:scale-x-100 md:scale-y-100" />
+        <img
+          src={ArrowIcon}
+          alt="Arrow Icon"
+          className="w-15 h-15 pt-2 pl-2 pb-0 md:pt-10 md:pl-40 md:pb-5 transform scale-x-50 scale-y-50 md:scale-x-100 md:scale-y-100"
+        />
       </div>
 
       <div className="flex items-center justify-end">
@@ -29,11 +66,31 @@ const Hero = () => {
 
       <p className="font-manrope text-TextoGrande pt-5 md:pt-0 md:text-TituloMediano text-blackText leading-tight">
         What are we{' '}
-        <span className="font-zillaslab text-primary">
-          <br /> creating
-        </span>{' '}
-        today?{' '}
+        <br />
+        <span
+          className="blink-vertical cursor-line font-museomoderno-100 font-thin text-greyText"
+          style={{
+            display: 'inline-block',
+            animation: 'blink-vertical 0.75s step-end infinite',
+          }}
+        >
+          |
+        </span>
+        <span className="font-zillaslab text-primary">{dynamicText}</span> today?
       </p>
+
+      <style>
+        {`
+          @keyframes blink-vertical {
+            from, to {
+              opacity: 0;
+            }
+            50% {
+              opacity: 1;
+            }
+          }
+        `}
+      </style>
 
       <div className="flex flex-col items-end pt-10 md:pt-0 pb-8">
         <a
