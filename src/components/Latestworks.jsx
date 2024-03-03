b30b3cbc8931b50deed13382cc14b069631fca77
yeysonpupa <yeysonpupa@unisabana.edu.co>
2024-03-03 15:27:46 -0500

    Repair latestworks

--- src/components/Latestworks.jsx
+++ src/components/Latestworks.jsx
@@ -2,6 +2,11 @@ import { Link } from 'react-router-dom';
 import Dataprojects from '../Dataprojects';
 
 const LatestWorks = () => {
+    
+    const handleHover = () => {
+        setHovered(!hovered);
+    };
+    
     return (
         <div className="pr-8 pl-8 md:pr-16 md:pl-16 pt-8 bg-whiteBackground">
             <div className="flex items-center justify-between">
@@ -14,13 +19,12 @@ const LatestWorks = () => {
             </div>
 
             <div className="flex mt-8">
-                {/* Columna 1 */}
                 <div className="w-1/2 pr-4 flex flex-col">
                     <img
                         src={Dataprojects[1].imageThumbnail}
                         alt={Dataprojects[1].title}
-                        className="w-full mb-4 flex-1"
-                        style={{ objectFit: 'cover', height: '100%' }}
+                        className="w-full mb-2 flex-1"
+                        style={{ objectFit: 'cover', maxHeight: '100%' }}
                     />
                     <div className="flex items-center justify-between">
                         <p className="font-manrope text-TextoPequeñoPhone md:text-TextoNormal text-blackText">{Dataprojects[1].title}</p>
@@ -28,14 +32,11 @@ const LatestWorks = () => {
                     </div>
                 </div>
 
-                {/* Columna 2 */}
                 <div className="w-1/2 pl-4 flex flex-col">
-                    {/* Contenido de la parte superior */}
-                    <div className="mb-4 flex-1">
-                        <img src={Dataprojects[4].imageThumbnail} alt={Dataprojects[4].title} className="w-full h-full" style={{ objectFit: 'contain' }} />
+                    <div className="mb-2 flex-1">
+                        <img src={Dataprojects[4].imageThumbnail} alt={Dataprojects[4].title} className="w-full h-full" style={{objectFit: 'cover', maxHeight: '200px'}}/>
                     </div>
 
-                    {/* Contenido de la parte inferior */}
                     <div className="flex-1 flex flex-col justify-between">
                         <div className="mb-4">
                             <div className="flex items-center justify-between">
@@ -44,7 +45,7 @@ const LatestWorks = () => {
                             </div>
                         </div>
                         <div>
-                            <img src={Dataprojects[0].imageThumbnail} alt={Dataprojects[0].title} className="w-full mb-4 h-75" style={{ objectFit: 'cover' }} />
+                            <img src={Dataprojects[0].imageThumbnail} alt={Dataprojects[0].title} className="w-full mb-2 h-full" style={{objectFit: 'cover', maxHeight: '200px'}}/>
                             <div className="flex items-center justify-between">
                                 <p className="font-manrope text-TextoPequeñoPhone md:text-TextoNormal text-blackText">{Dataprojects[0].title}</p>
                                 <p className="font-manrope text-TextoPequeñoPhone md:text-TextoNormal text-greyText">{Dataprojects[0].typeproject}</p>
