2b02aa90a8c88f5bc36df0ea920284c6138cccd7
yeysonpupa <yeysonpupa@unisabana.edu.co>
2024-03-03 16:16:21 -0500

    Finish latest works

--- src/components/Latestworks.jsx
+++ src/components/Latestworks.jsx
@@ -1,14 +1,27 @@
 import { Link } from 'react-router-dom';
 import Dataprojects from '../Dataprojects';
+import { useState } from 'react';
 
 const LatestWorks = () => {
-    
-    const handleHover = () => {
-        setHovered(!hovered);
+
+    const [hoveredImage1, setHoveredImage1] = useState(false);
+    const [hoveredImage2, setHoveredImage2] = useState(false);
+    const [hoveredImage3, setHoveredImage3] = useState(false);
+
+    const handleHoverImage1 = () => {
+        setHoveredImage1(!hoveredImage1);
+    };
+
+    const handleHoverImage2 = () => {
+        setHoveredImage2(!hoveredImage2);
+    };
+
+    const handleHoverImage3 = () => {
+        setHoveredImage3(!hoveredImage3);
     };
     
     return (
-        <div className="pr-8 pl-8 md:pr-16 md:pl-16 pt-8 bg-whiteBackground">
+        <div className="pr-8 pl-8 md:pr-16 md:pl-16 pt-6 bg-whiteBackground">
             <div className="flex items-center justify-between">
                 <p className="font-manrope text-TextoMedianoPhone md:text-TituloNormal text-blackText">
                     Latest Projects<span className="text-TextoMedianoPhone md:text-TituloNormal text-primary font-museomoderno">.</span>
@@ -18,14 +31,23 @@ const LatestWorks = () => {
                 </Link>
             </div>
 
-            <div className="flex mt-8">
+            <div className="flex mt-4 md:mt-8">
                 <div className="w-1/2 pr-4 flex flex-col">
-                    <img
-                        src={Dataprojects[1].imageThumbnail}
-                        alt={Dataprojects[1].title}
-                        className="w-full mb-2 flex-1"
-                        style={{ objectFit: 'cover', maxHeight: '100%' }}
-                    />
+                    <div
+                        className="relative h-full mb-2"
+                        style={{
+                            overflow: 'hidden',
+                        }}
+                    >
+                        <img
+                            src={Dataprojects[1].imageThumbnail}
+                            alt={Dataprojects[1].title}
+                            className="w-full h-full flex-1"
+                            style={{ objectFit: 'cover', maxHeight: '100%', transform: `scale(${hoveredImage1 ? 1.1 : 1})`, transition: 'transform 0.3s ease', }}
+                            onMouseEnter={handleHoverImage1}
+                            onMouseLeave={handleHoverImage1}
+                        />
+                    </div>
                     <div className="flex items-center justify-between">
                         <p className="font-manrope text-TextoPequeñoPhone md:text-TextoNormal text-blackText">{Dataprojects[1].title}</p>
                         <p className="font-manrope text-TextoPequeñoPhone md:text-TextoNormal text-greyText">{Dataprojects[1].typeproject}</p>
@@ -34,7 +56,20 @@ const LatestWorks = () => {
 
                 <div className="w-1/2 pl-4 flex flex-col">
                     <div className="mb-2 flex-1">
-                        <img src={Dataprojects[4].imageThumbnail} alt={Dataprojects[4].title} className="w-full h-full" style={{objectFit: 'cover', maxHeight: '200px'}}/>
+                        <div
+                            className="relative h-full"
+                            style={{
+                            overflow: 'hidden',
+                            }}
+                        >
+                            <img 
+                                src={Dataprojects[4].imageThumbnail} alt={Dataprojects[4].title} 
+                                className="w-full h-full" 
+                                style={{objectFit: 'cover', maxHeight: '200px', transform: `scale(${hoveredImage2 ? 1.1 : 1})`, transition: 'transform 0.3s ease', }}
+                                onMouseEnter={handleHoverImage2}
+                                onMouseLeave={handleHoverImage2}
+                            />
+                        </div>
                     </div>
 
                     <div className="flex-1 flex flex-col justify-between">
@@ -45,7 +80,24 @@ const LatestWorks = () => {
                             </div>
                         </div>
                         <div>
-                            <img src={Dataprojects[0].imageThumbnail} alt={Dataprojects[0].title} className="w-full mb-2 h-full" style={{objectFit: 'cover', maxHeight: '200px'}}/>
+                            <div className="mb-2 flex-1">
+                                <div
+                                    className="relative h-full"
+                                    style={{
+                                    overflow: 'hidden',
+                                    }}
+                                 >
+                                    <img 
+                                        src={Dataprojects[0].imageThumbnail}
+                                        alt={Dataprojects[0].title} 
+                                        className="w-full h-full" 
+                                        style={{objectFit: 'cover', maxHeight: '200px', transform: `scale(${hoveredImage3 ? 1.1 : 1})`, transition: 'transform 0.3s ease', }}
+                                        onMouseEnter={handleHoverImage3}
+                                        onMouseLeave={handleHoverImage3}
+                                    />
+                                </div>
+                            </div>
+
                             <div className="flex items-center justify-between">
                                 <p className="font-manrope text-TextoPequeñoPhone md:text-TextoNormal text-blackText">{Dataprojects[0].title}</p>
                                 <p className="font-manrope text-TextoPequeñoPhone md:text-TextoNormal text-greyText">{Dataprojects[0].typeproject}</p>
