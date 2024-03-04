45b465a1a9a571fa584a29a21e57198ece9aa6e4
yeysonpupa <yeysonpupa@unisabana.edu.co>
2024-03-04 18:33:45 -0500

    Try 2 Netlify

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import Firma from '../assets/images/firma';
 import UpButton from '../UI/UpButton';
 
 const Footer = () => {
@@ -7,11 +6,7 @@ const Footer = () => {
   return (
     <div className="relative flex flex-col items-center justify-center pt-8 md:pt-16">
       <div className="flex flex-col items-center">
-        <img
-          src={Firma}
-          alt="Firma Icon"
-          className="w-36 h-auto md:w-80 mb-1 md:mb-2"
-        />
+        
 
         <h1 className="font-manrope text-TextoMedianoPhone md:text-TituloNormal text-blackText mb-1 md:mb-2">
           {`Let's Connect`}
