0fd31ebb79491e20b493e0c1c655a5dd505bd097
yeysonpupa <yeysonpupa@unisabana.edu.co>
2024-03-06 19:10:55 -0500

    Final cursor

--- src/UI/Cursor.jsx
+++ src/UI/Cursor.jsx
@@ -22,7 +22,7 @@ export default function Cursor() {
     const tl = gsap.timeline({ paused: true });
 
     tl.to(curs.current, { height: "56px", width: "56px", ease: "expo.inout", opacity: "1" })
-      .to(textView.current?.style, { opacity: 1, ease: "expo.inout" }, 0);
+      .to(textView.current, { opacity: 1, ease: "expo.inout" }, 0);
 
     elementsWithEffect.forEach((element) => {
       element.addEventListener("mouseenter", function () {
@@ -51,16 +51,14 @@ export default function Cursor() {
     });
 
     function moveCursor(e) {
-      if (curs.current) {
-        const maxX = window.innerWidth - curs.current.offsetWidth;
-        const maxY = window.innerHeight - curs.current.offsetHeight;
-    
-        const newX = Math.min(Math.max(e.pageX, 0), maxX);
-        const newY = Math.min(Math.max(e.pageY - window.scrollY, 0), maxY);
-    
-        setCursor((prevCursor) => ({ ...prevCursor, x: newX, y: newY }));
-      }
-    }    
+      const maxX = window.innerWidth - curs.current.offsetWidth;
+      const maxY = window.innerHeight - curs.current.offsetHeight;
+
+      const newX = Math.min(Math.max(e.pageX, 0), maxX);
+      const newY = Math.min(Math.max(e.pageY - window.scrollY, 0), maxY);
+
+      setCursor((prevCursor) => ({ ...prevCursor, x: newX, y: newY }));
+    }
 
     document.addEventListener("mousemove", moveCursor);
 
@@ -79,12 +77,11 @@ export default function Cursor() {
   return shouldRenderCursor ? (
     <div
       ref={curs}
-      className={`cursor-none stroke-none pointer-events-none opaciti-1 fixed rounded-full transform -translate-x-1/2 -translate-y-1/2 bg-primary h-3 w-3`}
+      className={`cursor-none stroke-none pointer-events-none opacity-1 fixed rounded-full transform -translate-x-1/2 -translate-y-1/2 bg-primary h-3 w-3`}
       style={{
         left: `${x}px`,
         top: `${y}px`,
         position: "fixed",
-        zIndex: "9999",
       }}
     >
       <div
@@ -99,6 +96,7 @@ export default function Cursor() {
           fontWeight: "600",
           opacity: 0,
           transition: "opacity 0.3s",
+          zIndex: "9999",
         }}
       >
         View
